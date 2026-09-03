c6254fbda07d2dabc14c3956ecab8668bfd63f77
Abir Hasan <abirhasan5208@gmail.com>
2026-09-03 23:05:10 +0600

    feat: initialize Prisma and configure Express server

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import httpStatus from "http-status";
+import config from "./app/config";
+
+const app: Application = express();
+
+app.use(
+  cors({
+    origin: config.frontend_url,
+    credentials: true,
+  }),
+);
+
+// Enable URL-encoded form data parsing
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cookieParser());
+
+// Basic route
+app.get("/", async (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "Welcome to  GridFlow Backed Server",
+  });
+});
+
+export default app;
